290aa29809d413ef75729c92a007d346cc13bbf6
aarondesu <aaronneielaro@gmail.com>
2025-06-20 15:36:52 +0800

    refactor: get monthly consumption instead of reading

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,7 +5,6 @@ use App\Models\Invoice;
 use App\Models\Meter;
 use App\Models\MeterReading;
 use App\Models\Subscriber;
-use Illuminate\Database\QueryException;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
@@ -13,84 +12,104 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        try {
-            // Get number of subscribers
-            $subscribers = Subscriber::get()->count();
+        // Get number of subscribers
+        $subscribers = Subscriber::get()->count();
 
-            // Get number of meters
-            $meters        = Meter::get()->count();
-            $meters_active = Meter::where('status', 'active')->count();
+        // Get number of meters
+        $meters        = Meter::get()->count();
+        $meters_active = Meter::where('status', 'active')->count();
 
-            // Get total consumption of water
-            $consumption = MeterReading::select(
-                DB::raw('SUM(reading) as total_reading'),
-                                                                    //DB::raw("MONTHNAME(created_at) as month"), MYSQL
-                DB::raw("TO_CHAR(created_at, 'FMMonth') as month"), // POSTGRES,
-                DB::raw('EXTRACT(month from created_at) as month_date')
-            )
-                ->whereBetween('created_at', [Carbon::now()->subMonths(12), Carbon::now()])
-                ->groupBy('month', 'month_date')
-                ->orderBy('month_date')
-                ->get();
+        // Get total consumption of water
+        $consumption = DB::select("
+                WITH consumption_per_row AS (
+                    SELECT
+                        meter_id,
+                        created_at,
+                        TO_CHAR(created_at, 'FMMonth') as month,
+                        EXTRACT(month from created_at) as month_date,
+                        COALESCE(reading - LAG(reading) OVER (PARTITION BY meter_id ORDER BY created_at), reading) as consumption
+                    FROM meter_readings
+                )
+                SELECT
+                    month,
+                    month_date,
+                    SUM(consumption) AS total_consumption
+                FROM consumption_per_row
+                GROUP BY month, month_date
+                ORDER BY month_date
+        ");
 
-            $current_reading = MeterReading::select(
-                DB::raw('SUM(reading) as total_reading'),
-                DB::raw("TO_CHAR(created_at, 'FMMonth') as month"),
-            )
-                ->whereYear('created_at', Carbon::now()->year)
-                ->whereMonth('created_at', Carbon::now()->month)
-                ->groupBy('month')
-                ->first();
+        $current_reading = MeterReading::select(
+            DB::raw('SUM(reading) as total_reading'),
+            DB::raw("TO_CHAR(created_at, 'FMMonth') as month"),
+        )
+            ->whereYear('created_at', Carbon::now()->year)
+            ->whereMonth('created_at', Carbon::now()->month)
+            ->groupBy('month')
+            ->first();
 
-            $previous_reading = MeterReading::select(
-                DB::raw('SUM(reading) as total_reading'),
-                DB::raw("TO_CHAR(created_at, 'FMMonth') as month"),
-            )
-                ->whereYear('created_at', Carbon::now()->year)
-                ->whereMonth('created_at', Carbon::now()->subMonth()->month)
-                ->groupBy('month')
-                ->first();
+        $previous_reading = MeterReading::select(
+            DB::raw('SUM(reading) as total_reading'),
+            DB::raw("TO_CHAR(created_at, 'FMMonth') as month"),
+        )
+            ->whereYear('created_at', Carbon::now()->year)
+            ->whereMonth('created_at', Carbon::now()->subMonth()->month)
+            ->groupBy('month')
+            ->first();
 
-            // Get Invoices
-            $invoices = Invoice::select(
-                DB::raw("TO_CHAR(created_at, 'FMMonth') as month"),
-                DB::raw('SUM(amount_due) as total_amount_due')
-            )
-                ->groupBy('month')
-                ->whereYear('created_at', Carbon::now()->year)
-                ->get();
+        $latest_readings = MeterReading::select(['id', 'reading', 'created_at', 'meter_id'])->with(['meter' => function ($query) {
+            $query->select(['id', 'number', 'subscriber_id']);
+        }])->with(['meter.subscriber' => function ($query) {
+            $query->select(['id', 'first_name', 'last_name']);
+        }])
+            ->orderByDesc('created_at')
+            ->limit(10)
+            ->get();
 
-            $total_amount_due = Invoice::select(
-                DB::raw("TO_CHAR(created_at, 'FMMonth') as month"),
-                DB::raw('SUM(amount_due) as total_amount_due'),
-            )
-                ->whereYear('created_at', Carbon::now()->year)
-                ->whereMonth('created_at', Carbon::now()->month)
-                ->groupBy("month")
-                ->get();
+        // Get Invoices
+        $invoices = Invoice::select(
+            DB::raw("TO_CHAR(created_at, 'FMMonth') as month"),
+            DB::raw('SUM(amount_due) as total_amount_due')
+        )
+            ->groupBy('month')
+            ->whereYear('created_at', Carbon::now()->year)
+            ->get();
 
-            return response()->json(['success' => true, 'data' => [
-                'total_subscribers' => $subscribers,
-                'meters'            => [
-                    'total'  => $meters,
-                    'active' => $meters_active,
-                ],
-                'readings'          => [
-                    'current_reading'  => $current_reading,
-                    'previous_reading' => $previous_reading,
-                    'list'             => $consumption,
-                ],
-                'invoice'           => [
-                    'current_amount_due' => $total_amount_due,
-                    'lsit'               => $invoices,
-                ],
-            ]]);
-        } catch (QueryException $queryException) {
-            return response()->json(['success' => false, 'errors' => [
-                'code'     => $queryException->getCode(),
-                'message'  => $queryException->getMessage(),
-                'bindings' => $queryException->getBindings(),
-            ]], 500);
-        }
+        $current_invoice = Invoice::select(
+            DB::raw("TO_CHAR(created_at, 'FMMonth') as month"),
+            DB::raw('SUM(amount_due) as total_amount_due'),
+        )
+            ->whereYear('created_at', Carbon::now()->year)
+            ->whereMonth('created_at', Carbon::now()->month)
+            ->groupBy("month")
+            ->first();
+
+        return response()->json(['success' => true, 'data' => [
+            'total_subscribers' => $subscribers,
+            'meters'            => [
+                'total'  => $meters,
+                'active' => $meters_active,
+            ],
+            'readings'          => [
+                'current_reading'  => $current_reading,
+                'previous_reading' => $previous_reading,
+                'list'             => $consumption,
+                'latest'           => $latest_readings,
+            ],
+            'invoice'           => [
+                'current_invoice' => $current_invoice,
+                'list'            => $invoices,
+            ],
+        ]]);
+
+        // try {
+
+        // } catch (QueryException $queryException) {
+        //     return response()->json(['success' => false, 'errors' => [
+        //         'code'     => $queryException->getCode(),
+        //         'message'  => $queryException->getMessage(),
+        //         'bindings' => $queryException->getBindings(),
+        //     ]], 500);
+        // }
     }
 }
